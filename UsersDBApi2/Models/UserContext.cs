0cc6c9934c6a2d1716d6014e8eb81d2efbba4bf3
roman.harmatiuk <roman.harmatiuk@UKEM0087>
2021-10-25 19:46:39 +0300

    Add project files.

---
+++ UsersDBApi2/Models/UserContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersDBApi2.Models
+{
+    public class UserContext: DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+    : base(options)
+        {
+        }
+
+        //public DbSet<User> UserItems { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
